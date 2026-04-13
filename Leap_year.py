6252463f3c5395a75ff0a38d711a50aa8ee03662
Emmanuel Orubele <blontf@gmail.com>
2026-04-13 17:27:56 -0400

    Leap Year code

--- Leap_year.py
+++ Leap_year.py
@@ -1,27 +1,21 @@
 def is_leap(year):
     leap = False
-    a = 4
-    b = 100
-    c = 400
-    result = 0
-    # Write your logic here
-    if ((year % a) == result): # check if year is divisible by 4
-        if (year % b) == result: # check if year is divisible by 100
-                leap = False # if year is divisible by 100, it is not a leap year
-        elif (year % c) == result: # check if year is divisible by 400
-                leap = True # if year is divisible by 400, it is a leap year
-        #continue
-        print(year%a)
-
-    elif (year % a) == result: #check if year is divisible by 4
-        if (year % b) == result: # check if year is divisible by 100
-                leap = False # if year is divisible by 100, it is not a leap year
-                if (year % c) == result: # check if year is divisible by 400
-                    leap = True # if year is divisible by 400, it is a leap year
-                    
-        #print(year % a)
-        
+    
+    # Divisible by 4?
+    if year % 4 == 0:
+        # But if it's divisible by 100...
+        if year % 100 == 0:
+            # It must also be divisible by 400 to be a leap year
+            if year % 400 == 0:
+                leap = True
+            else:
+                leap = False
+        else:
+            # Divisible by 4 but not 100
+            leap = True
+            
     return leap
+
 
 year = int(input())
 print(is_leap(year))
