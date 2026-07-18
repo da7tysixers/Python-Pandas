9c4a2c9e45feadda5a6e38f8003cbf949439e708
Emmanuel Orubele <blontf@gmail.com>
2026-07-18 11:06:19 -0400

    PowerShell course

---
+++ powershell_course.ps1
@@ -0,0 +1,13 @@
+ New-AzResourceGroup -Name {Name} -Location {Location}
+
+ #Clear the screen
+    Clear-Host
+
+#Check Version of PowerShell
+    $PSVersionTable.PSVersion
+    $PSVersionTable
+
+
+# Command basics
+# Verb-noun
+get-help get-verb | more
