bac646065adb59b10fe3eaff558789f015715512
Emmanuel Orubele <blontf@gmail.com>
2026-07-26 21:06:36 -0400

    Folder structure changes to python course

---
+++ WeatherTemp.py
@@ -0,0 +1,20 @@
+temperature = 91
+"""
+if temperature > 90 or temperature < 95:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature <= 50:
+    print("It's a cold day")
+else:
+    print("It's not a hot day")
+"""
+temperature = 85
+forcast = "sunny"
+
+if temperature > 80 and forcast == "sunny":
+    if forcast == "sunny":
+        print("It's a hot day")
+    print("Stay hydrated")
+elif temperature < 80 and forcast == "rainy":
+        print("It's a cold day")
+
